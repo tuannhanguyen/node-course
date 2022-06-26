c0945d2bf50a85ae44bc327432389c7de990e9c4
tuannhan <tuannhan6@gmail.com>
2022-06-26 16:36:02 +0700

    object property shorthand

---
+++ playground/5-es6-object.js
@@ -0,0 +1,28 @@
+const name = 'Nhan'
+const userAge = 22
+
+// const user = {
+//     name,
+//     age: userAge,
+//     location: 'saigon'
+// }
+
+// console.log(user)
+
+const product = {
+    label: 'samsung a52',
+    price: 3,
+    stock: 200,
+    salePrice: undefined,
+    //rating: 4.2
+}
+
+// const {label: labelProduct, stock, rating = 5} = product
+
+// console.log(rating)
+
+const tracsaction = (type, {label, stock}) => {
+    console.log(type, label, stock)
+}
+
+tracsaction('order', product)
