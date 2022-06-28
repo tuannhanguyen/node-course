552ec09b33d9c95466fae0e895f5f5185c166b39
tuannhan <tuannhan6@gmail.com>
2022-06-28 21:50:16 +0700

    query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -43,9 +43,25 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
     res.send({
-        forecast: 'It is snowing',
-        location: 'Sapa'
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
