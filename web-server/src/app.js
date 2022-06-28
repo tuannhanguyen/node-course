e34999aaac37f381ccb95777b98071f30940da90
tuannhan <nhan.nvtn@tctav.com>
2022-06-28 11:38:44 +0700

    dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,9 +5,38 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Set up handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+
+// Set up static directory to server
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Nguyen Vo Tuan Nhan'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Nguyen Vo Tuan Nhan'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'This is some helpful text'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
