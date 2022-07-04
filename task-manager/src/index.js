23ceca1be5cec5bc1d1e337c4e855032361a0d74
tuannhan <nhan.nvtn@tctav.com>
2022-07-04 16:54:57 +0700

    Resource Updating and Deleting Endpoints

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -43,6 +43,43 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({'error' : 'Invalid updates'})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true } )
+        
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 
 app.post('/tasks', async (req, res) => {
    const task = new Task(req.body)
@@ -51,7 +88,7 @@ app.post('/tasks', async (req, res) => {
         await task.save()
         res.status(201).send(task)
    } catch (error) {
-        res.status(500).send(error)
+        res.status(400).send(error)
    }
 })
 
@@ -73,7 +110,45 @@ app.get('/tasks/:id', async (req, res) => {
         if (!task) {
             return res.status(404).send()
         }
-        res.status(200).send(task)
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'compledted']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
     } catch (error) {
         res.status(500).send(error)
     }
