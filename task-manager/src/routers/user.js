9773778ea7733fd34a7eb2fc9697e359b093f192
tuannhan <nhan.nvtn@tctav.com>
2022-07-05 10:40:13 +0700

    Logging in users

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -13,6 +13,16 @@ router.post('/users', async (req, res) => {
     }
 })
 
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)    
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+    
+})
+
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
@@ -49,6 +59,7 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true } )
 
         const user = await User.findById(req.params.id)
 
@@ -58,7 +69,6 @@ router.patch('/users/:id', async (req, res) => {
 
         await user.save()
 
-        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true } )
         if (!user) {
             return res.status(404).send()
         }
